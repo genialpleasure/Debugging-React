0c7ab79d93c8ed4dc9586f9f6ca329efad7adbae
genialpleasure <isaacnpleasure01@gmail.com>
2025-10-26 03:25:00 +0100

    debuggeds

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,8 +2,13 @@ import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 
 export default function Results({ input }) {
-  const results = [];
+  const results = []; 
   calculateInvestmentResults(input, results);
+
+  if (results.length === 0) {
+    return <p className='center' style={{color : 'red'}}>Invalid Input</p>
+  }
+
   const initialInvestment =
     results[0].valueEndOfYear -
     results[0].interest -
